b946cbb847a7bd874535e82721a095c3f193e16a
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-14 20:24:21 -0500

    Created a programm that asked the user in the terminal for the birthday.

--- src/com/itavarez/Main.java
+++ src/com/itavarez/Main.java
@@ -1,8 +1,16 @@
 package com.itavarez;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("When is your birthday?");
+
+        Scanner scanner = new Scanner(System.in);
+        int birthday = scanner.nextInt();
+
+        System.out.println("My birthday is " + birthday);
+
     }
 }
